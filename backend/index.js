f032492bc5b210a5079a57cfd1affeaf50d5d309
unknown <ashraf>
2024-10-29 08:48:43 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import AuthRoutes from './routes/Auth.routes.js'
 import DbCon from './libs/db.js'
+import cors from "cors";
 dotenv.config()
 const PORT=process.env.PORT || 4000
 const app=express()
+
+app.use(cors())
 
 DbCon()
 app.use(express.json())
